783123482f015a38b36c153ef2ea24828089dd86
saeed-mohammad <sk.saeed008@gmail.com>
2026-09-21 17:46:26 +0530

    Initial Commit

---
+++ MiniCalmApp/MiniCalmAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniCalmAppApp.swift
+//  MiniCalmApp
+//
+//  Created by saeed shaikh on 21/09/26.
+//
+
+import SwiftUI
+
+@main
+struct MiniCalmAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
